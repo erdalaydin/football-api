d15800bc6d6410b2cd120e2420242bc976fb1cf5
erdalaydin <erdalaydin92@hotmail.com>
2019-09-04 12:29:26 +0200

    schema built

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db')
 const port = process.env.PORT || 4000
+const model = require('./team/model')
 
 
 app.listen(port, () => console.log(`you can visit ${port} in the server`));
